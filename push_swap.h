15aecc365d55cca29d8bef6f808c785b2516f7ea
adaza-ru <adaza-ru@student.42malaga.com>
2026-02-25 02:07:37 +0100

    add: argv error handling

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adaza-ru <adaza-ru@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adaza-ru <adaza-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 17:30:29 by adaza-ru          #+#    #+#             */
-/*   Updated: 2026/02/13 17:40:59 by adaza-ru         ###   ########.fr       */
+/*   Updated: 2026/02/25 00:40:53 by adaza-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,24 @@
 typedef struct s_stack
 {
 	int				number;
+	int				index;
 	struct s_stack	*next;
 	struct s_stack	*prev;
 }		t_stack;
 
-
+/*
+typedef enum e_state
+{
+	START,
+	PARSING,
+	LOADING,
+	SORT_SMALL,
+	SORT_FIVE,
+	SORT_BIG,
+	DONE,
+	ERROR
+}	t_state;
+*/
+int check_argv_error(char **argv);
 
 #endif
